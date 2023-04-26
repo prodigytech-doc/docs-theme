6220d46a792401a2521f627e47f6951d4aa621e4
yuechao.xu <yuechao.xu@appshahe.com>
2023-04-26 11:30:53 +0800

    feat: 去掉无用的指令

--- scripts/release.js
+++ scripts/release.js
@@ -76,16 +76,6 @@ async function main() {
   step('\nBuilding the package...')
   await run('npm', ['run', 'build'])
 
-  const { yes: changelogOk } = await prompts({
-    type: 'confirm',
-    name: 'yes',
-    message: `Changelog generated. Does it look good?`
-  })
-
-  if (!changelogOk) {
-    return
-  }
-
   // Commit changes to the Git and create a tag.
   step('\nCommitting changes...')
   await run('git', ['add', 'package.json'])
